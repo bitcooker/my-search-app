05e16acdb1a4069b14befa2b7c483aa9c4c9ff45
bitcooker <dreamhighpassionhigh@gmail.com>
2022-08-10 10:07:36 -0700

    update styles for buttons

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,17 +4,21 @@ import { BiSearchAlt2 } from "react-icons/bi";
 import { BiMicrophone } from "react-icons/bi";
 
 function Search() {
+
+    const search = e => {
+        e.preventDefault();
+    }
   return (
     <div className="search">
       <div className="search__input">
-        <BiSearchAlt2 className="search__input-icon"/>
+        <BiSearchAlt2 className="search__input-icon" />
         <input />
         <BiMicrophone />
       </div>
 
       <div className="search__buttons">
-<button className="btn search1">Search</button>
-<button className="btn lucky">I'm feeling Lucky</button>
+        <button className="btn search1" onClick={search}>Search</button>
+        <button className="btn lucky">I'm feeling Lucky</button>
       </div>
     </div>
   );
