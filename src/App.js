07993f2f94714db7b6136adc53c2d8513157a3e5
bitcooker <dreamhighpassionhigh@gmail.com>
2022-08-10 08:04:39 -0700

    i react-icons, react-router-dom, add Home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="app">
       <h1>Let's build a search clone!</h1>
+      {/* Home (the one with the search on) */}
+      <Home />
+
+      {/* Search page (the results page) */}
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='home__header'>
+      <div className='home__header-left'>
+
+      </div>
+      <div className='home__header-right'>
+        
+      </div>
+      </div>
+      <div className='home__body'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
