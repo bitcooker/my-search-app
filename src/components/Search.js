8f42a54758d7ba3e936cee9a43fcd9e61813169b
bitcooker <dreamhighpassionhigh@gmail.com>
2022-08-11 01:03:18 -0700

    update enter key and search btn triggers input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 import { BiSearchAlt2 } from "react-icons/bi";
 import { BiMicrophone } from "react-icons/bi";
 
 function Search() {
+const [input, setInput] = useState('');
 
     const search = e => {
         e.preventDefault();
+        //make sure preventing the refresh
+        console.log('U HIT THE SEARCH BTN', input)
     }
+
   return (
-    <div className="search">
+    <form className="search">
       <div className="search__input">
         <BiSearchAlt2 className="search__input-icon" />
-        <input />
+        <input value={input} onChange={e => setInput(e.target.value)}/>
         <BiMicrophone />
       </div>
 
       <div className="search__buttons">
-        <button className="btn search1" onClick={search}>Search</button>
+        <button className="btn search1" type="submit" onClick={search}>Search</button>
         <button className="btn lucky">I'm feeling Lucky</button>
       </div>
-    </div>
+    </form>
   );
 }
 
