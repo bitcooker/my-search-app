4c31a339643515af4c7aeee91c8a93b15c34d31e
bitcooker <dreamhighpassionhigh@gmail.com>
2022-08-11 02:27:42 -0700

    create SearchPage.css, show term

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useStateValue } from "../StateProvider";
+import "./SearchPage.css";
 
 function SearchPage() {
+  const [{ term }, dispatch] = useStateValue();
+
   return (
-    <div>
-      Search page
+    <div className="searchPage">
+      <div className="searchPage__header">
+        <h1>{term}</h1>
+      </div>
+      <div className="searchPage__results"></div>
     </div>
-  )
+  );
 }
 
-export default SearchPage
+export default SearchPage;
