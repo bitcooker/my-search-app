cda0a271e5bea56fef6b115315ff815ea61d0d39
bitcooker <dreamhighpassionhigh@gmail.com>
2022-08-11 05:28:28 -0700

    add .env

--- src/keys.js
+++
@@ -1,3 +0,0 @@
-export const API_KEY = 'AIzaSyAcxQLaHUMBDwP6gjfOyAFq8GYSLCGY9H0';
-
-export default API_KEY;
--- src/useGSearch.js
+++ src/useGSearch.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import API_KEY from "./keys";
 
 const CONTEXT_KEY = "8081f0f719ff94b8b";
 
@@ -9,7 +8,7 @@ const useGSearch = (term) => {
   useEffect(() => {
     const fetchData = async () => {
       fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
       )
         .then((response) => response.json())
         .then((result) => {
